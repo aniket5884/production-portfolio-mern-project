5c2c4100ba8b62c680ce07a8011708309848eb5b
aniket5884 <sharmaaniket5884@gmail.com>
2025-04-21 01:51:33 +0530

    production app
    mern stack productioin portfolio app 2025

---
+++ controllers/portfolioController.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer')
+const sendGridTransport = require('nodemailer-sendgrid-transport')
+
+//transport
+const transporter = nodemailer.createTransport(
+    sendGridTransport({
+        auth:{
+            api_key: process.env.API_SENDGRID,
+        },
+    })
+);
+const sendEmailController = (req, res) => {
+    try{
+        const {name,email,msg} = req.body
+
+        //validation
+        if(!name || !email || !msg){
+            return res.status(500).send({
+                success:false,
+                message:'Please Provide All Fields',
+            });
+        }
+        //email matter
+        transporter.sendMail({
+            to:"sharmaaniket5884@gmail.com",
+            from:"sharmaaniket5884@gmail.com",
+            subject:'Regarding Mern Portfolio App',
+            html:`
+            <h5>Detail Information</>
+            <ul>
+            <li><p>Name : ${name}</p><li>
+            <li><p>Email : ${email}</p><li>
+            <li><p>Message : ${msg}</p><li>
+            </ul>
+        `
+        })
+        return resizeBy.status(200).send({
+            success: true,
+            message: "Your Message Send Successfully",
+        });
+    } catch (error){
+         console.log(error)
+         return resizeBy.status(500).send({
+            success: false,
+            message: 'Send Email API Error',
+            error,
+         });
+    }
+};
+
+module.exports = { senEmailController };
